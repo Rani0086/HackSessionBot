3f32eb23ea05984453944684c3f0529e5b239fa6
ishu9805 <abhishek47903@gmail.com>
2024-06-21 08:56:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
-    API_ID = 
-    API_HASH = ""
-    TOKEN = ""  
-    START_PIC = "" 
-    CHAT = ""    
+    API_ID = 26626068
+    API_HASH = "bf423698bcbe33cfd58b11c78c42caa2"
+    TOKEN = "7461505284:AAFsqEwWww1GZakSa9oLyalchIY03PTLQu8"  
+    START_PIC = "https://telegra.ph/file/3f73818323ec008dc01f0.jpg" 
+    CHAT = "-1002048925723"    
