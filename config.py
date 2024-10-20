5719c549ceba978f0772fecfc607cc8c1dcaebab
Rani0086 <shantidhaakad@gmail.com>
2024-10-20 13:38:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
-    API_ID = 26626068
-    API_HASH = "bf423698bcbe33cfd58b11c78c42caa2"
-    TOKEN = "7461505284:AAFsqEwWww1GZakSa9oLyalchIY03PTLQu8"  
+    API_ID = "23255238"
+    API_HASH = "009e3d8c1bdc89d5387cdd8fd182ec15"
+    TOKEN = "7942473848:AAG60u5qIud7CrJtwdEd4k2UtJfMwzlw6ME"  
     START_PIC = "https://telegra.ph/file/3f73818323ec008dc01f0.jpg" 
-    CHAT = "-1002048925723"    
+    CHAT = "-1002306515099"    
